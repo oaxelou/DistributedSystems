aa817fee9b61535308cd897b14764cffeea034c5
eirini5th <etsitsopl@hotmail.com>
2019-03-03 23:41:01 +0200

    atMostOnceThread, locks added need to check non-blocking sockets

--- hw1/temp_client.py
+++ hw1/temp_client.py
@@ -1,51 +1,136 @@
 import socket
+import time
+import random
+import threading
+from threading import Thread
+from threading import Lock
+import fcntl, os
+import errno
+
 
 MCAST_GRP = '224.0.0.1'
 MCAST_PORT = 10000
 SERVICEID = 1
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+PERIOD = 2
 
+lock = Lock()
+requests2send = {}
+end_lifetime = 0
 
-print("Socket is ready")
+################################################################################
+# client sender thread code
+class AtMostOnceSender(Thread):
+    def run(self):
+        while 1:
+            time.sleep(PERIOD)
+            if end_lifetime == 1:
+                print("Exiting from snder thread")
+                break
+            lock.acquire()
+            print("Printing list of requests to send", requests2send)
+            for request in requests2send.keys():
+                print("Going to send request", request)
+                sock.sendto(str(requests2send[request]).encode(), (MCAST_GRP, MCAST_PORT))
+            lock.release()
+################################ APP FUNCTIONS #################################
+def app():
 
-# sock.sendto("Hello World", (MCAST_GRP, MCAST_PORT))
-requestID = 0
+    block = 0
 
-while 1:
-    try:
-        int2check = input("int2check: ")
-    except KeyboardInterrupt:
-        print("Ending communication...")
-        break
+    while 1:
+        try:
+            int2check = input("int2check: ")
+            # int2check = file_input[i]
 
-    if not int2check:
-        print("Ending communication...")
-        break
+        except KeyboardInterrupt:
+            sock.close()
+            end_lifetime = 1
+            print("Ending communication...")
+            exit()
+
+        if not int2check:
+            sock.close()
+            end_lifetime = 1
+            print("Ending communication...")
+            exit()
+
+        requestID = sendRequest(SERVICEID, int2check)
+        # while getReply(requestID, block) == 1:
+        #     time.sleep(1)
+        #     print("will retry to get")
+        # time.sleep(1)
+        getReply(requestID, block)
+
+def sendRequest(svcid, int2check):
+    # sendRequest.__dict__.setdefault('atMostOnceThread', -1)
+    #
+    # if sendRequest.atMostOnceThread == -1:
+    #     sendRequest.atMostOnceThread = AtMostOnceSender()
+    #     sendRequest.atMostOnceThread.start()
+
+    sendRequest.__dict__.setdefault('requestID', -1)
+    sendRequest.requestID += 1
+
 
     # probably won't be needed (99%)
     try:
-        message2send = (SERVICEID, requestID, int(int2check))#, int(int2check)) #"ADD_SERVER"
+        message2send = (svcid, sendRequest.requestID, int(int2check))
     except ValueError as verror:
-        message2send = (SERVICEID, requestID, int2check)
+        message2send = (svcid, sendRequest.requestID, int2check)
 
+    lock.acquire()
+    print("Adding to requests2send: ", message2send)
+    requests2send[sendRequest.requestID] = message2send
+    lock.release()
+    # print("added ",message2send)
+    return sendRequest.requestID
 
-    print("Going to send: ", )
-    sock.sendto(str(message2send).encode(), (MCAST_GRP, MCAST_PORT))
-    print("Sent ",message2send)
-
+def getReply(requestID, block):
     print("Going to wait")
     try:
         d = sock.recvfrom(1024)
     except KeyboardInterrupt:
+        end_lifetime = 1
+        atMostOnceThread.join()
+        sock.close()
         print("Ending temp client/slave-server")
-        break
+        exit()
+    except socket.error as e:
+        err = e.args[0]
+        if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
+            print("there is no reply and getReply is nonBlocking")
+            return 1
+        else:
+            print("serious error", e)
+            return 2
 
     data = d[0]
     addr = d[1]
 
     print("Message[" + addr[0] + ":" + str(addr[1]) + "] : " + data.decode().strip())
-    requestID = requestID + 1
 
-sock.close()
+    lock.acquire()
+    if requestID in requests2send:
+        print(requestID, "has been received and will be removed from list")
+        requests2send.pop(requestID)
+    else:
+        print("Request ", requestID   , " not found")
+    print(requests2send)
+    lock.release()
+    return 0
+
+############################### SOCKET CREATION ################################
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+
+fcntl.fcntl(sock, fcntl.F_SETFL, os.O_NONBLOCK)
+
+print("Socket is ready")
+
+################################################################################
+atMostOnceThread = -1
+atMostOnceThread = AtMostOnceSender()
+atMostOnceThread.start()
+app()
