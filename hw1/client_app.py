4378811ccbf74d29fae4247b4dc15ebd4c418fdc
eirini5th <etsitsopl@hotmail.com>
2019-03-08 21:55:32 +0200

    added a few comments - seems ok

--- hw1/client_app.py
+++ hw1/client_app.py
@@ -1,6 +1,21 @@
+# Distributed Systems - hw1 :
+# Asychronous request-reply with at most once and dynamic management of the servers
+#                                                        (load balancing)
+# Axelou Olympia 2161
+# Tsitsopoulou Irene 2203
+#
+# client_app.py: Client app file
+#
+#
+# arguments:
+#   blocking: 0 for non-blocking receiving / 1 for blocking
+#   batch size: number of requests to send first, and then wait replies of all of them
+#   sleep time: if a line "s" is given as input, the client sleeps for the amount given as
+#               argument. If sleep time is given, but "s" is not given as input, sleep time
+#               will be ignored
+
 from client_library import *
 
-# could be moved to _library, but it is only used in _app
 import sys
 
 SERVICEID = 1
@@ -8,10 +23,9 @@ REQ_TIME_GROUP = 5
 
 ################################ APP FUNCTION ##################################
 def main():
-    # global end_lifetime
     global sock
     if len(sys.argv) > 4 or len(sys.argv) < 2:
-        print("Wrong no of args. argv[1]: blocking (0/1), argv[2]: testcase, argv[3]: sleep time")
+        print("Wrong no of args. argv[1]: blocking (0/1), argv[2]: batch size, argv[3]: sleep time")
         exit()
 
     if int(sys.argv[1]) not in [0,1]:
@@ -30,41 +44,152 @@ def main():
     noOfSuccessReq = {}
     interval = -1
 
-    while 1:
-        for i in range(0, batch):
+    if batch != 0:
+        while 1:
+            for i in range(0, batch):
+                try:
+                    int2check = input("int2check: ")
+                    if int2check == "s" and len(sys.argv) == 4:
+                        time.sleep(sleep_time)
+                        continue
+                except KeyboardInterrupt:
+                    sock.close()
+                    print("KeyboardInterrupt: Ending communication...")
+                    exit()
+                # when reading from an input file, client collects replies when reaching EOF
+                except EOFError:
+                    while 1:
+                        if not req2wait4:
+                            break
+                        # check if any of the requests sent has got a reply from server
+                        for req in list(req2wait4.keys()):
+                            replies_got = 0
+                            (getReplyError, answer) = getReply(req, block)
+                            if getReplyError == SUCCESS:
+                                # count no of successful replies and calculate delay since sending
+                                delay[req] = time.time() - sendRequestTime[req]
+                                interval = int(req / REQ_TIME_GROUP)
+                                total_delay[interval] += delay[req]
+                                noOfSuccessReq[interval] += 1
+                                # make groups of #REQ_TIME_GROUP requests to calculate mean delay time
+                                if req % REQ_TIME_GROUP == 0:
+                                    mean = total_delay[interval] / noOfSuccessReq[interval]
+                                    print( "-> ", time.time(), "\ttotal delay: ", total_delay[interval], "\tmean total delay: ", mean, file=sys.stderr)
+
+                                print("\t\t@@@@@@@@@@@@@@ APPLICATION:  ", req2wait4[req], ": ", answer)
+                                del req2wait4[req]
+                                del sendRequestTime[req]
+                                del delay[req]
+                            elif getReplyError == EXPIRED_ERROR:
+                                print("\t\t", req2wait4[req], " has expired. Going to remove it from list")
+                                del req2wait4[req]
+                            elif getReplyError == NO_SERVER_ERROR:
+                                print("\t\tNo server available for ", req2wait4[req], ". Going to remove it from list")
+                                del req2wait4[req]
+
+                            # break when #batch number of replies have been collected
+                            replies_got += 1
+                            if replies_got == batch:
+                                break
+
+                    sock.close()
+                    print("total delay in each interval:")
+                    print(total_delay)
+                    print("mean total delay in each interval:")
+                    for interval in total_delay:
+                        try:
+                            print(interval, ":", total_delay[interval] / noOfSuccessReq[interval])
+                        except ZeroDivisionError:
+                            print(interval, ": No replies")
+
+                    print("Ending communication...")
+                    exit()
+                    break
+                # close communication when enter is pressed
+                if not int2check:
+                    sock.close()
+                    print("total delay in each interval:")
+                    print(total_delay)
+                    print("mean total delay in each interval:")
+                    for interval in total_delay:
+                        try:
+                            print(interval, ":", total_delay[interval] / noOfSuccessReq[interval])
+                        except ZeroDivisionError:
+                            print(interval, ": No replies")
+
+                    print("Ending communication...")
+                    exit()
+
+                print(" ")
+                requestID = sendRequest(SERVICEID, int2check)
+                # make groups of #REQ_TIME_GROUP requests to calculate mean delay time
+                if requestID % REQ_TIME_GROUP == 0:
+                    interval += 1
+                    total_delay[interval] = 0
+                    noOfSuccessReq[interval] = 0
+                sendRequestTime[requestID] = time.time()
+                req2wait4[requestID] = int2check
+
+            # check if any of the requests sent has got a reply from server
+            for req in list(req2wait4.keys()):
+                (getReplyError, answer) = getReply(req, block)
+                if getReplyError == SUCCESS:
+                    delay[req] = time.time() - sendRequestTime[req]
+                    interval = int(req / REQ_TIME_GROUP)
+                    total_delay[interval] += delay[req]
+                    noOfSuccessReq[interval] += 1
+                    if req % REQ_TIME_GROUP == 0:
+                        mean = total_delay[interval] / noOfSuccessReq[interval]
+                        print( "-> ", time.time(), "\ttotal delay: ", total_delay[interval], "\tmean total delay: ", mean, file=sys.stderr)
+
+                    print("\t\t@@@@@@@@@@@@@@ APPLICATION:  ", req2wait4[req], ": ", answer)
+                    del req2wait4[req]
+                    del sendRequestTime[req]
+                    del delay[req]
+
+                elif getReplyError == EXPIRED_ERROR:
+                    print("\t\t", req2wait4[req], " has expired. Going to remove it from list")
+                    del req2wait4[req]
+                elif getReplyError == NO_SERVER_ERROR:
+                    print("\t\tNo server available for ", req2wait4[req], ". Going to remove it from list")
+                    del req2wait4[req]
+
+################################# DEFAULT ######################################
+
+    else:
+        while 1:
             try:
                 int2check = input("int2check: ")
                 if int2check == "s" and len(sys.argv) == 4:
-                    time.sleep(sleep_time)
+                    time.sleep(int(sys.argv[3]))
+                    continue
+                elif int2check == "s" and len(sys.argv) != 4:
                     continue
             except KeyboardInterrupt:
                 sock.close()
-                print("KeyboardInterrupt: Ending communication...")
+                print("Ending communication...")
                 exit()
             except EOFError:
-                print("eof error")
                 while 1:
-                    # print("again")
                     if not req2wait4:
                         break
                     for req in list(req2wait4.keys()):
-                        replies_got = 0
                         (getReplyError, answer) = getReply(req, block)
                         if getReplyError == SUCCESS:
-                            # print("\t\tFOUND ONE IN EOF")
+
                             delay[req] = time.time() - sendRequestTime[req]
                             interval = int(req / REQ_TIME_GROUP)
                             total_delay[interval] += delay[req]
                             noOfSuccessReq[interval] += 1
                             if req % REQ_TIME_GROUP == 0:
                                 mean = total_delay[interval] / noOfSuccessReq[interval]
-                                print("#sucessful reqs: ", noOfSuccessReq[interval])
                                 print( "-> ", time.time(), "\ttotal delay: ", total_delay[interval], "\tmean total delay: ", mean, file=sys.stderr)
 
                             print("\t\t@@@@@@@@@@@@@@ APPLICATION:  ", req2wait4[req], ": ", answer)
                             del req2wait4[req]
                             del sendRequestTime[req]
                             del delay[req]
+
                         elif getReplyError == EXPIRED_ERROR:
                             print("\t\t", req2wait4[req], " has expired. Going to remove it from list")
                             del req2wait4[req]
@@ -72,24 +197,18 @@ def main():
                             print("\t\tNo server available for ", req2wait4[req], ". Going to remove it from list")
                             del req2wait4[req]
 
-                        replies_got += 1
-                        if replies_got == batch:
-                            break
-
                 sock.close()
-                # print("mean total delay = ", total_delay/noOfSuccessReq)
-                print("Eof: total delay in each interval:")
+                print("total delay in each interval:")
                 print(total_delay)
-                print("Eof: mean total delay in each interval:")
+                print("mean total delay in each interval:")
                 for interval in total_delay:
                     try:
                         print(interval, ":", total_delay[interval] / noOfSuccessReq[interval])
                     except ZeroDivisionError:
                         print(interval, ": No replies")
-
-                print("EofEnding communication...")
-                exit()
+                print("Ending communication...")
                 break
+
             if not int2check:
                 sock.close()
                 print("total delay in each interval:")
@@ -100,163 +219,43 @@ def main():
                         print(interval, ":", total_delay[interval] / noOfSuccessReq[interval])
                     except ZeroDivisionError:
                         print(interval, ": No replies")
-
-                print("Ending communication...")
-                # print("mean total delay = ", total_delay/noOfSuccessReq)
                 print("Ending communication...")
                 exit()
 
-            print("sending: ", int2check)
+            print("")
             requestID = sendRequest(SERVICEID, int2check)
+
             if requestID % REQ_TIME_GROUP == 0:
                 interval += 1
                 total_delay[interval] = 0
                 noOfSuccessReq[interval] = 0
+
             sendRequestTime[requestID] = time.time()
-            # print("\n\n", req2wait4, "\n\n")
             req2wait4[requestID] = int2check
+            for req in list(req2wait4.keys()):
+                (getReplyError, answer) = getReply(req, block)
 
-        for req in list(req2wait4.keys()):
-            print("req: ", req)
-            (getReplyError, answer) = getReply(req, block)
+                if getReplyError == SUCCESS:
+                    delay[req] = time.time() - sendRequestTime[req]
+                    interval = int(req / REQ_TIME_GROUP)
+                    total_delay[interval] += delay[req]
+                    noOfSuccessReq[interval] += 1
+                    if req % REQ_TIME_GROUP == 0:
+                        mean = total_delay[interval] / noOfSuccessReq[interval]
+                        print( "-> ", time.time(), "\ttotal delay: ", total_delay[interval], "\tmean total delay: ", mean, file=sys.stderr)
 
-            if getReplyError == SUCCESS:
-                print("\t\tFOUND ONE IN REGULAR getReply")
+                    print("\t\t@@@@@@@@@@@@@@ APPLICATION:  ", req2wait4[req], ": ", answer)
+                    del req2wait4[req]
+                    del sendRequestTime[req]
+                    del delay[req]
 
-                delay[req] = time.time() - sendRequestTime[req]
-                interval = int(req / REQ_TIME_GROUP)
-                total_delay[interval] += delay[req]
-                noOfSuccessReq[interval] += 1
-                if req % REQ_TIME_GROUP == 0:
-                    mean = total_delay[interval] / noOfSuccessReq[interval]
-                    print("#sucessful reqs: ", noOfSuccessReq[interval])
-                    print( "-> ", time.time(), "\ttotal delay: ", total_delay[interval], "\tmean total delay: ", mean, file=sys.stderr)
+                elif getReplyError == EXPIRED_ERROR:
+                    print("\t\t", req2wait4[req], " has expired. Going to remove it from list")
+                    del req2wait4[req]
+                elif getReplyError == NO_SERVER_ERROR:
+                    print("\t\tNo server available for ", req2wait4[req], ". Going to remove it from list")
+                    del req2wait4[req]
 
-                print("\t\t@@@@@@@@@@@@@@ APPLICATION:  ", req2wait4[req], ": ", answer)
-                del req2wait4[req]
-                del sendRequestTime[req]
-                del delay[req]
-
-            elif getReplyError == EXPIRED_ERROR:
-                print("\t\t", req2wait4[req], " has expired. Going to remove it from list")
-                del req2wait4[req]
-            elif getReplyError == NO_SERVER_ERROR:
-                print("\t\tNo server available for ", req2wait4[req], ". Going to remove it from list")
-                del req2wait4[req]
-
-#############################
-# mporoume na afhsoume to default gia batch = 0
-
-################################# DEFAULT ######################################
-
-    while 1:
-        try:
-            int2check = input("int2check: ")
-            if int2check == "s" and len(sys.argv) == 4:
-                time.sleep(int(sys.argv[3]))
-                continue
-            elif int2check == "s" and len(sys.argv) != 4:
-                continue
-        except KeyboardInterrupt:
-            sock.close()
-            print("Ending communication...")
-            exit()
-        except EOFError:
-            while 1:
-                if not req2wait4:
-                    break
-                for req in list(req2wait4.keys()):
-                    (getReplyError, answer) = getReply(req, block)
-                    if getReplyError == SUCCESS:
-
-                        delay[req] = time.time() - sendRequestTime[req]
-                        interval = int(req / REQ_TIME_GROUP)
-                        total_delay[interval] += delay[req]
-                        noOfSuccessReq[interval] += 1
-                        if req % REQ_TIME_GROUP == 0:
-                            mean = total_delay[interval] / noOfSuccessReq[interval]
-                            print("#sucessful reqs: ", noOfSuccessReq[interval])
-                            print( "-> ", time.time(), "\ttotal delay: ", total_delay[interval], "\tmean total delay: ", mean, file=sys.stderr)
-
-                        print("\t\t@@@@@@@@@@@@@@ APPLICATION:  ", req2wait4[req], ": ", answer)
-                        del req2wait4[req]
-                        del sendRequestTime[req]
-                        del delay[req]
-
-                    elif getReplyError == EXPIRED_ERROR:
-                        print("\t\t", req2wait4[req], " has expired. Going to remove it from list")
-                        del req2wait4[req]
-                    elif getReplyError == NO_SERVER_ERROR:
-                        print("\t\tNo server available for ", req2wait4[req], ". Going to remove it from list")
-                        del req2wait4[req]
-
-            sock.close()
-            # print("mean total delay = ", total_delay/noOfSuccessReq)
-            print("total delay in each interval:")
-            print(total_delay)
-            print("mean total delay in each interval:")
-            for interval in total_delay:
-                try:
-                    print(interval, ":", total_delay[interval] / noOfSuccessReq[interval])
-                except ZeroDivisionError:
-                    print(interval, ": No replies")
-
-            print("Ending communication...")
-            break
-        if not int2check:
-            sock.close()
-            print("total delay in each interval:")
-            print(total_delay)
-            print("mean total delay in each interval:")
-            for interval in total_delay:
-                try:
-                    print(interval, ":", total_delay[interval] / noOfSuccessReq[interval])
-                except ZeroDivisionError:
-                    print(interval, ": No replies")
-
-            print("Ending communication...")
-            # print("mean total delay = ", total_delay/noOfSuccessReq)
-            print("Ending communication...")
-            exit()
-
-        requestID = sendRequest(SERVICEID, int2check)
-
-        if requestID % REQ_TIME_GROUP == 0:
-            interval += 1
-            total_delay[interval] = 0
-            noOfSuccessReq[interval] = 0
-
-
-        sendRequestTime[requestID] = time.time()
-        # print("\n\n", req2wait4, "\n\n")
-        req2wait4[requestID] = int2check
-        for req in list(req2wait4.keys()):
-            print("req: ", req)
-            (getReplyError, answer) = getReply(req, block)
-
-            if getReplyError == SUCCESS:
-                delay[req] = time.time() - sendRequestTime[req]
-                interval = int(req / REQ_TIME_GROUP)
-                total_delay[interval] += delay[req]
-                noOfSuccessReq[interval] += 1
-                if req % REQ_TIME_GROUP == 0:
-                    mean = total_delay[interval] / noOfSuccessReq[interval]
-                    print("#sucessful reqs: ", noOfSuccessReq[interval])
-                    print( "-> ", time.time(), "\ttotal delay: ", total_delay[interval], "\tmean total delay: ", mean, file=sys.stderr)
-
-                print("\t\t@@@@@@@@@@@@@@ APPLICATION:  ", req2wait4[req], ": ", answer)
-                del req2wait4[req]
-                del sendRequestTime[req]
-                del delay[req]
-
-            elif getReplyError == EXPIRED_ERROR:
-                print("\t\t", req2wait4[req], " has expired. Going to remove it from list")
-                del req2wait4[req]
-            elif getReplyError == NO_SERVER_ERROR:
-                print("\t\tNo server available for ", req2wait4[req], ". Going to remove it from list")
-                del req2wait4[req]
-
-        # time.sleep(1)
 
 
 
