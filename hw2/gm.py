1a7044849b0ed3917e2108e63122486393b3bfc3
oaxelou <oaxelou@inf.uth.gr>
2019-03-20 15:39:52 +0200

    Added group dicts. All set up except for the mutlicast (inform others on new member's join)

--- hw2/gm.py
+++ hw2/gm.py
@@ -5,22 +5,36 @@ import time
 from ast import literal_eval as make_tuple
 from random import randint
 
+RED    = '\033[91m'
+GREEN  = '\033[92m'
+YELLOW = '\033[93m'
+BLUE   = '\033[94m'
+ENDC   = '\033[0m'
+
 NON_PRIVILEGED_TCP_PORTS_START = 1024 #non-privileged ports: > 1023
 TCP_PORT = 5016 #non-privileged ports: > 1023
 tcp_ports_being_used = []
 
-MCAST_GRP = '224.0.0.1'
-MCAST_PORT = 10000
+GM_MCAST_ADDR = '224.0.0.1'
+GM_MCAST_PORT = 10000
 
-REQUEST_MES_SIZE = 20  # Normally 1024, but we want fast response
+REQUEST_MES_SIZE = 1000  # Normally 1024, but we want fast response
+
+
+GRPS_MCAST_ADDR = '224.0.0.2'
+GRPS_MCAST_PORT = 1024
+grp_ports_being_used = []
+# initialization of groups dictionary
+groups = {}
 
 # UDP socket initialization
 udp_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 udp_s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 udp_s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+mreq = struct.pack("4sl", socket.inet_aton(GM_MCAST_ADDR), socket.INADDR_ANY)
 udp_s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-udp_s.bind((MCAST_GRP, MCAST_PORT))
+udp_s.bind((GM_MCAST_ADDR, GM_MCAST_PORT))
+################################################################################
 
 def find_first_available_tcp_port():
     print("\nSearch for the first available tcp port:")
@@ -30,67 +44,114 @@ def find_first_available_tcp_port():
     print("First available tcp_port: ", tcp_port)
     tcp_ports_being_used.append(tcp_port)
     return tcp_port
-
+########################################################
 def remove_tcp_port(tcp_port):
     if tcp_port in tcp_ports_being_used:
         tcp_ports_being_used.remove(tcp_port)
         print(tcp_port, " Successfully removed from list")
     else:
         print(tcp_port, " not in list")
+
+def find_first_available_grp_port():
+    print("\nSearch for the first available tcp port:")
+    grp_port = NON_PRIVILEGED_TCP_PORTS_START
+    while grp_port in grp_ports_being_used:
+        grp_port += 1
+    print("First available grp_port: ", grp_port)
+    grp_ports_being_used.append(grp_port)
+    return grp_port
+########################################################
+def remove_grp_port(grp_port):
+    if grp_port in grp_ports_being_used:
+        grp_ports_being_used.remove(grp_port)
+        print(grp_port, " Successfully removed from grp list")
+    else:
+        print(grp_port, " not in grp list")
 ################################################################################
+
 def establish_tcp_conn():
     while 1:
         print("\n\n\nGoing to wait for a udp message")
         d = udp_s.recvfrom(10)
+        # print("Received message from ", d[1])
         if d[0].decode() == "TCP":
             tcp_port = find_first_available_tcp_port()
             udp_s.sendto(str(tcp_port).encode(), d[1])
             tcp_user_ip_addr, _ = d[1]
-            # print("Going to init TCP socket")
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             s.bind((tcp_user_ip_addr, tcp_port))
-            # time.sleep(4)
             s.listen(1)
-            # print("After listening to socket")
-            # time.sleep(4)
             (conn, addr) = s.accept()
-            # print ('Connection address:', addr)
-
-            return (s, conn, tcp_port)
+            return (s, conn, tcp_user_ip_addr, tcp_port)
         else:
             print("Received unknown message. Waiting for another one")
-
-def close_tcp_connection(conn, s, tcp_port):
+########################################################
+def close_tcp_connection(conn, s, tcp_port, release_port_number):
     conn.close()
     s.close()
-    remove_tcp_port(tcp_port)
-########################################################
-def join(conn):
-    random_answer_generator = randint(0,1)
-    print("JOIN: going to return ", ("J-ACK" if (random_answer_generator == 1) else "J-N-ACK"))
-    if random_answer_generator:
-        conn.send("J-ACK".encode())
-    else:
-        conn.send("J-N-ACK".encode())
+    if release_port_number == True:
+        # Sto join den apodesmeuei to port epeidh ekei tha ginetai to polling!
+        remove_tcp_port(tcp_port)
+################################################################################
 
+def join(conn, args, user_ip, user_gm_tcp_port):
+    global groups
+    g_name, user_id = args
+    print(BLUE, '"', user_id, '" @', user_ip, ' wants to enter group "', g_name, '"', ENDC)
+    # random_answer_generator = randint(0,1)
+    # print("JOIN: going to return ", ("J-ACK" if (random_answer_generator == 1) else "J-N-ACK"))
+    # if random_answer_generator:
+    #     conn.send("J-ACK".encode())
+    # else:
+    #     conn.send("J-N-ACK".encode())
+
+    if g_name in groups:
+        # pass # NOT READY YET
+        users_no, grp_addr, users_dict = groups[g_name]
+        if user_id in users_dict:
+            print(RED, user_id, " already in ", g_name, ENDC)
+            conn.send(str(("J-N-ACK", 0)).encode())
+        else:
+            users_no += 1
+            users_dict[user_id] = (user_ip , user_gm_tcp_port, False)
+            # inform everyone that a new user joined the group
+            print(GREEN, user_id, " added in ", g_name)
+            print(groups, ENDC)
+            conn.send(str(("J-ACK",(users_no , grp_addr))).encode())
+    else:
+        # add group in groups
+        grp_addr = (GRPS_MCAST_ADDR, find_first_available_grp_port())
+        users_dict = {user_id: (user_ip , user_gm_tcp_port, True)}
+        # init Multicast socket for this group? here?
+        groups[g_name] = (1, grp_addr, users_dict)
+        print(GREEN, user_id, " added in ", g_name)
+        print(groups, ENDC)
+        conn.send(str(("J-ACK",(1 , grp_addr))).encode())
 ########################################################
 # GM code
 
 while 1:
-    (s, conn, tcp_port) = establish_tcp_conn()
+    (s, conn, user_ip, tcp_port) = establish_tcp_conn()
     print("\n\nTCP connection established!!")
 
-    data = conn.recv(REQUEST_MES_SIZE)
+    data = conn.recvfrom(REQUEST_MES_SIZE)
     print ("received data:", data)
-    if data.decode() == "JOIN":
-        join(conn)
+    print("From ", data[1])
+    # args: gname and user_id
+    command, args = make_tuple(data[0].decode())
+    if command == "JOIN":
+        join(conn, args, user_ip, tcp_port)
+        close_tcp_connection(conn, s, tcp_port, False)
+    elif command == "LEAVE":
+        # kalese thn leave
+        close_tcp_connection(conn, s, tcp_port, True)
     else:
         conn.send("WHAT?".encode())  # default answer when it doesn't know what to do
+        close_tcp_connection(conn, s, tcp_port, True)
 
     print ("sent data:", data)
 
-    close_tcp_connection(conn, s, tcp_port)
 
 ##################################
 # Never reaches here
--- hw2/user.py
+++ hw2/user.py
@@ -4,48 +4,72 @@ import time
 import struct
 from ast import literal_eval as make_tuple
 
-MCAST_GRP = '224.0.0.1'
-MCAST_PORT = 10000
+RED    = '\033[91m'
+GREEN  = '\033[92m'
+YELLOW = '\033[93m'
+BLUE   = '\033[94m'
+ENDC   = '\033[0m'
+
+GM_MCAST_ADDR = '224.0.0.1'
+GM_MCAST_PORT = 10000
 
 UDP_MES_SIZE = 100
-REQUEST_MES_SIZE = 100
+REQUEST_MES_SIZE = 1000
 MESSAGE = "Hello World"
 
 SUCCESS = True
 FAILURE = False
 
+# initialization of the groups I'm in
+groups = {}
+available_sock_id = 0
+gm_tcp_port = -1
+
 def establish_tcp_conn():
+    global gm_tcp_port
     # Handshake to establish TCP connection (via Multicast)
-    udp_s.sendto("TCP".encode(), (MCAST_GRP, MCAST_PORT))
+    udp_s.sendto("TCP".encode(), (GM_MCAST_ADDR, GM_MCAST_PORT))
     udp_address = udp_s.recvfrom(UDP_MES_SIZE)
     tcp_ip, _ = udp_address[1]
-    tcp_port = int(udp_address[0].decode())
+    gm_tcp_port = int(udp_address[0].decode())
     # Going to init TCP socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     while 1:
         print("Going to try to connect to TCP server")
         try:
-            s.connect((tcp_ip, tcp_port))
+            s.connect((tcp_ip, gm_tcp_port))
             print("Successfully connected to TCP")
             break
         except ConnectionRefusedError as notConnectedError:
-            time.sleep(1)
+            time.sleep(1) # isws na vgei meta
             print("Going to try again")
     return s
 
-def join():
+def join(gname, my_id):
+    global available_sock_id
+    global groups
     tcp_socket = establish_tcp_conn()
     # Ready to send request to GM!
-    tcp_socket.send("JOIN".encode())
+    args_to_send = (gname, my_id)
+    tcp_socket.send(str(("JOIN", args_to_send)).encode())
     print("Wait for GM to receive join request")
     data = tcp_socket.recv(REQUEST_MES_SIZE)
     print("Received data: ", data.decode())
     tcp_socket.close()
     # Check here if it's a success or a failure
-    if data.decode() == "J-ACK":
-        return SUCCESS
+    ack_code, ack_info = make_tuple(data.decode())
+    if ack_code == "J-ACK":
+        print("ack_info: ", ack_info)
+        users_no, multicast_addr = ack_info
+        if users_no == 1:
+            isSequencer = True
+        else:
+            isSequencer = False
+        groups[available_sock_id] = (gname, users_no, multicast_addr, isSequencer)
+        available_sock_id += 1
+        return available_sock_id - 1
     else:
-        return FAILURE
+        return -1
 #################################################
 
 # User code starts here
@@ -54,9 +78,14 @@ udp_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 udp_s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
 
-joined_group_successfully = join()
-if joined_group_successfully:
-    print("Successfully joined group")
+grp_id = input("Enter grp_id: ")
+user_id = input("Enter user_id: ")
+gsock_id = join(grp_id, user_id)
+if gsock_id >= 0:
+    print("Successfully joined group stored in sockid: ", gsock_id)
+    print(BLUE, "I can connect with GM through port ", gm_tcp_port, ENDC)
+    print("Groups I belong in:")
+    print(GREEN, groups, ENDC)
 else:
     print("Failed at joining group")
 
